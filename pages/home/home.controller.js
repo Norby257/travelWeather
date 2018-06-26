03155e147bab54379c9dc49503ee366126ea4308
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-06-26 16:35:14 -0500

    initial HomePage

---
+++ pages/home/home.controller.js
@@ -0,0 +1,12 @@
+//  wrapping code in IIFE 
+
+(function() {
+    'use strict';
+
+    angular.module('home').controller('HomeController', HomeController);
+
+    function HomeController() {
+        var vm = this;
+        //  more will be added here 
+    }
+})();
