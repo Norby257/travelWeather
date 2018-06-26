d463a44bcc0cf94577f9a5e3297d8124ddc9e8da
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-06-26 16:44:09 -0500

    geolocation added

---
+++ scripts/geolocation.js
@@ -0,0 +1,19 @@
+console.log('We are linked');
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        // //google maps api 
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        console.log("Geolocation is not supported by this browser.");
+    }
+
+}
+
+function showPosition(position) {
+    console.log("Latitude " + position.coords.latitude);
+}
+
+
+getLocation();
