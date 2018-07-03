408b641b07bb399386bbd77e8fd8a6303b6860c6
Noreen Madden <noreenmadden@Noreens-MacBook-Air.local>
2018-07-03 17:14:28 -0500

    created new forecast array data

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,14 +1,3 @@
-//  example of what you can do with weather service
-//  i.e. call the API
-//  get data
-//  store in array
-//  then ng- ng repeat
-//  use fn declarations
-//  following Jonh Papa
-//  controller handles user interaction with app
-//  actual HTTP call is in the service
-//  controller calls the service
-//  in template do the ng-click
 (function() {
   angular
     .module("App")
@@ -51,31 +40,38 @@
       return WeatherService.getDefaultWeather().then(function(data) {
         vm.title = data;
         // vm.icon  = "http://openweathermap.org/img/w/"+title.data.list[0].weather[0].icon +".png"
-  
-        //   find a more efficient way to do this so i don't have to type as much
+
         //  this should be a function
-        //   reduce risk of error by less typing
-          //   push data into array
-    
+        //   push data into array
+        // have to do the equivalent of for each -
         vm.forecasts.push(vm.title.data.list);
         console.log(vm.forecasts); // output: [Array(40)]
-        console.log(vm.forecasts[0]); // array 
-        console.log(vm.forecasts[0].length); // 
+        console.log(vm.forecasts[0]); // array
+        console.log(vm.forecasts[0].length); //
         //   loop thru array, grab every eighth element
-        //  these are objects 
+        //  these are objects
+        //   push the nested objects to a new array called 5-day forecast. it should contain an object for each day,
+        //  so there should be five elements total (i.e.) Mon, Tues, Wed, Thurs, Fri, Sat
+        var fiveDayForecast = [];
         for (let i = 0; i < vm.forecasts[0].length; i += 8) {
           console.log(vm.forecasts[0][i]);
-          
+          fiveDayForecast.push(vm.forecasts[0][i]);
+          console.log(fiveDayForecast);
 
-          //   these are just comments to test output is expected 
+          //   these are just comments to test output is expected
           //    console.log(vm.forecasts[0][i].weather[0]);
           // console.log(vm.forecasts[0][i].main)
         }
+        //  just want to quickly loop over this new array 
+        //  to see what is up in it 
+        console.log(fiveDayForecast);
 
-        //   push the nested objects to a new array called 5-day forecast. it should contain an object for each day 
+        for (let i = 0; i < fiveDayForecast.length; i++) {
+          console.log(fiveDayForecast[i]);
+        }
 
-        //   then we do ng-repeat on that new array
-
+        //   then we do ng-repeat on that new array (In HTML file)
+        //  ng-repeat  forecast in forecasts 
         //   I am confident a more efficient solution is out there - doing this for now bc it works, then will make it right, make it fast
       });
     }
