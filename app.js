d64cdf0492b38daa3f110d529e22b401a107abb0
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-15 19:17:22 -0500

    updates before merging branch

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 (function() {
-angular.module('App', [])
+angular.module('App', ['ngRoute'])
 //  going to need thre controllers 
 //  one for weather 
 //  one for getting the city input 
 //  one for getting the user's location? 
 
-
+// app.config(function($routeProvider){
+//     .when('/', {
+//         templateUrl: 'pages/Home/home.htm',
+//         controller: 'HomeController'
+//     })
+// })
 //  going to need two pages 
 //  home page 
 //  forecast page 
